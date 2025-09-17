4e6a02c57e70e5b19b1ce4d3caefd9c00770f567
JoaoVitor365 <rm94269@estudante.fieb.edu.br>
2025-09-17 09:12:33 -0300

    Busca Produtos

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,12 +1,14 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
 const Produtos = () => {
-
-    // Objeto da lista de pizzas
-
-    const pizzas = [
-        'Pizza Muçarela',
-        'Pizza Calabresa',
-        'Pizza Portuguesa', 'Pizza Baiana', 'Pizza Frita'
-    ]
+    const [pizzas, setPizzas] = useState([])
+    // Consumir rota com lista de produtos
+    axios.get("http://viacep.com.br/ws/01010000/json")
+    .then(response=>{
+        console.log(response.data)
+    })
+    .catch(error=>{console.log(error)})
 
     // Iteração da lista de pizzas - pizza a pizza (um a um)
     const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
